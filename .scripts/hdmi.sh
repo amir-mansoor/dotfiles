55b80f1c94097245c731fa32f2c1d67e64a8fb08
amir-mansoor <qazimansor89@gmail.com>
2026-05-11 17:15:34 +0500

    script for hdmi connecting in i3

---
+++ .scripts/hdmi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+INTERNAL="eDP-1"
+
+EXTERNAL=$(xrandr | grep " connected" | grep -v "$INTERNAL" | cut -d" " -f1 | head -n1)
+
+if [ -n "$EXTERNAL" ]; then
+    xrandr --output "$EXTERNAL" --auto --right-of "$INTERNAL"
+fi
--- .scripts/memory.sh
+++
@@ -1,1 +0,0 @@
-free -h | awk '/^Mem:/ {print $3 "/" $2}'
