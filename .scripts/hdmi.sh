565863899ae1e2c3f142442d23457649e4843c59
amir-mansoor <qazimansor89@gmail.com>
2026-05-11 17:21:14 +0500

    duplicate the screen on external display

--- .scripts/hdmi.sh
+++ .scripts/hdmi.sh
@@ -5,5 +5,5 @@ INTERNAL="eDP-1"
 EXTERNAL=$(xrandr | grep " connected" | grep -v "$INTERNAL" | cut -d" " -f1 | head -n1)
 
 if [ -n "$EXTERNAL" ]; then
-    xrandr --output "$EXTERNAL" --auto --right-of "$INTERNAL"
+    xrandr --output "$EXTERNAL" --auto --same-as "$INTERNAL"
 fi
